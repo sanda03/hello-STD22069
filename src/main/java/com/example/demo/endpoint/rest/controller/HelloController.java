64f6af77645f1a3f7ce1a6f3a9e2c045251a5fba
sanda03 <128069441+sanda03@users.noreply.github.com>
2025-02-12 20:09:32 +0300

    chore: add an endpoint hello

--- src/main/java/com/example/demo/endpoint/rest/controller/HelloController.java
+++ src/main/java/com/example/demo/endpoint/rest/controller/HelloController.java
@@ -1,1 +1,20 @@
+package com.example.demo.endpoint.rest.controller.health;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@AllArgsConstructor
+public class HelloController {
+
+  public static final ResponseEntity<String> OK = new ResponseEntity<>("OK", HttpStatus.OK);
+  public static final ResponseEntity<String> KO =
+      new ResponseEntity<>("KO", HttpStatus.INTERNAL_SERVER_ERROR);
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "world";
+  }
+}
